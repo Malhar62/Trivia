3bc8d507304ce7a07b3740abb57408d5bf8fda25
Malhar62 <80893407+Malhar62@users.noreply.github.com>
2022-12-12 10:19:21 +0530

    Update profile-screen.tsx

--- app/screens/profile/profile-screen.tsx
+++ app/screens/profile/profile-screen.tsx
@@ -121,7 +121,7 @@ export const ProfileScreen = observer(function ProfileScreen() {
           top: 0,
           left: 0,
           right: 0,
-          backgroundColor: 'navy',
+          backgroundColor: 'blue',
           overflow: 'hidden',
           height: length.interpolate({
             inputRange: [0, SCROLL],
@@ -210,3 +210,4 @@ export const ProfileScreen = observer(function ProfileScreen() {
           <Text style={styles.title}>Title</Text>
         </Animated.View>
  */
+//changes done by Y
